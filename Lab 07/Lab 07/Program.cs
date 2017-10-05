774b601b180233ed7e73672775d368c9b1d5b47c
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 15:58:27 +0700

    W07P12

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -10,6 +10,37 @@ namespace Lab_07
     {
         static void Main(string[] args)
         {
+
         }
     }
+    class Professor : Teacher
+        {
+            private float emolument;  // เงินประจำตำแหน่ง
+
+            public Professor(string name, float billingRate) : base(name, billingRate)
+            {
+            }
+
+            public Professor(string name, float billingRate, float emolument)
+            : this(name, billingRate)
+            {
+                this.emolument = emolument;
+            }
+
+            // new function, because it's different than the base version
+            public new float CalculateCharge(float hours)
+            {
+                if (hours < 1.0F)
+                    hours = 1.0F; // minimum charge.
+                return (hours * billingRate) + emolument;
+            }
+            // new function, because it's different than the base version
+            public new string TypeName()
+            {
+                return ("Professor");
+            }
+        }
+
+    }
 }
+
