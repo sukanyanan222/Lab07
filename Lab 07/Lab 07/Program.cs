9de348beda24561ed2d2b7fb903b1805bbf0196d
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 15:55:11 +0700

    W07P11

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -8,10 +8,25 @@ namespace Lab_07
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
+            Console.ReadKey();
         }
+
+      
     }
+
 
     class Person
     {
