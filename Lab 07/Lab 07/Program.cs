77ef241731b319215206dd3f6e63aa69e3fe18b3
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 14:54:27 +0700

    W07P08

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -9,16 +9,22 @@ namespace Lab_07
     
         class Teacher
         {
-            static void Main(string[] args)
-            {
-                Teacher teacher = new Teacher("Tom", 350f);
-                // teacher work for 20Hr/month
-                Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
-                    teacher.CalculateCharge(20f));
-                Console.ReadLine();
-            }
-            // constructor (for initial private/protected variables)
-            public Teacher(string name, float billingRate)
+
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Tom", 350f);
+            Professor prof = new Professor("Jerry", 500f, 5000f);
+            // teacher work 20Hr/month
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
+                teacher.CalculateCharge(20f));
+            // professor work 12Hr/month
+            Console.WriteLine("{0} charge = {1}", prof.TypeName(),
+                prof.CalculateCharge(12F));
+            Console.ReadLine();
+        }
+
+        // constructor (for initial private/protected variables)
+        public Teacher(string name, float billingRate)
             {
                 this.name = name;
                 this.billingRate = billingRate;
