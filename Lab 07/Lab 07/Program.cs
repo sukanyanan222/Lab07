c03fcc93567237fd8eb7f7e90a48373739c09d7c
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 15:10:15 +0700

    W07P10

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Lab_07
 {
-    class Program
-    {
-        static void Main(string[] args)
+class Program
         {
+            static void Main(string[] args)
+            {
+                Vector v1 = new Vector(1, 4);
+                Vector v2 = new Vector(8, 5);
+                Vector v3 = v1 + v2;
+                Console.WriteLine("vector v1 = " + v1);
+                Console.WriteLine("vector v2 = " + v2);
+                Console.WriteLine("vector v3 = " + v3);
+                Console.WriteLine("-v3 = " + (-v3));
+                Console.ReadKey();
+            }
         }
-    }
+        class Vector
+        {
+            public Vector(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            public int X { get; set; }  // X - accessor (property)
+            public int Y { get; set; }  // Y - accessor (property)
+
+            public override string ToString()
+            {
+                return string.Format("({0},{1})", X, Y);
+            }
+            // All operator overloads must be public and static.
+            public static Vector operator +(Vector v1, Vector v2) //binary operator +
+            {
+                return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+            }
+            public static Vector operator -(Vector v1)  //unary operator -
+            {
+                return new Vector(-v1.X, -v1.Y);
+            }
+      }
+
 }
+
