83f248bc2b985fb3ed677b092ed6267fa9c67886
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 13:27:13 +0700

    W07P04

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -10,6 +10,37 @@ namespace Lab_07
     {
         static void Main(string[] args)
         {
+            Student su = new Student();
+            su.Name = "Student Name";
+            su.ID = "12345678";
+            su.GPA = 3.5f;
+            Console.WriteLine("Student name : " + su.Name);
+            Console.WriteLine("Student ID   : " + su.ID);
+            Console.WriteLine("Student GPA  : " + su.GPA);
+            Console.ReadLine();
         }
     }
+    class Student
+    {
+        private string name;
+        private string id;
+        private float gpa;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public float GPA
+        {
+            get { return gpa; }
+            set { gpa = value; }
+        }
+    }
+
 }
+
