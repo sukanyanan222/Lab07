82c9ff56a700de7a94e2624ea98ce54b69aaa0b0
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 13:31:59 +0700

    W07P05

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -10,6 +10,54 @@ namespace Lab_07
     {
         static void Main(string[] args)
         {
+            Student su = new Student();
+            try
+            {
+                su.Name = "Student Name";
+                su.ID = "12345678";
+                su.GPA = 7.5f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
+    class Student
+    {
+        private string name;
+        private string id;
+        private float gpa;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public float GPA
+        {
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
+        }
+    }
+
 }
+    
+
