64c912175764d19ea99dd494f229aa8cb03aa75b
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 15:06:07 +0700

    W07P09

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Lab_07
 {
-    class Program
-    {
-        static void Main(string[] args)
+  class Program
+            
         {
+            void print(int i)
+            {
+                Console.WriteLine("Printing int: {0}", i);
+            }
+
+            void print(double f)
+            {
+                Console.WriteLine("Printing float: {0}", f);
+            }
+
+            void print(string s)
+            {
+                Console.WriteLine("Printing string: {0}", s);
+            }
+
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                p.print(5);
+                p.print((5.26f).ToString());
+                p.print(264.2871);
+                p.print((int)1354.23);
+                p.print("Hello World of C#");
+                Console.ReadKey();
+            }
         }
-    }
-}
+
+ }
