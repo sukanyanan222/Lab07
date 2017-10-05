656ba7c547c1083e8caa5a773ea198e6ae7a0a99
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 13:33:35 +0700

    W07P05

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -15,7 +15,7 @@ namespace Lab_07
             {
                 su.Name = "Student Name";
                 su.ID = "12345678";
-                su.GPA = 7.5f;
+                su.GPA = 3.5f;
                 Console.WriteLine("Student name : " + su.Name);
                 Console.WriteLine("Student ID   : " + su.ID);
                 Console.WriteLine("Student GPA  : " + su.GPA);
