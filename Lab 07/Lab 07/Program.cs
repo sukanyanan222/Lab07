d15f4e8bf67f02b0b41875cd7e9c18c393b4fb1a
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 13:54:24 +0700

    W07P07

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Lab_07
 {
-    class Program
+
+    class Teacher
     {
         static void Main(string[] args)
         {
+            Teacher teacher = new Teacher("Tom", 350f);
+            // teacher work for 20Hr/month
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
+                teacher.CalculateCharge(20f));
+            Console.ReadLine();
         }
+        // constructor (for initial private/protected variables)
+        public Teacher(string name, float billingRate)
+        {
+            this.name = name;
+            this.billingRate = billingRate;
+        }
+        // figure out the charge based on teacher's rate
+        public float CalculateCharge(float hours)
+        {
+            return (hours * billingRate);
+        }
+        // return the name of this type
+        public string TypeName()
+        {
+            return ("Teacher");
+        }
+        private string name;
+        protected float billingRate;
     }
 }
