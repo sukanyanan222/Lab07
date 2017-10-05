70c1597f0ad98649d5b2511405ba749c172f2af8
Sukanya <Sukanyak21130@gmail.com>
2017-10-05 15:15:46 +0700

    W07P11

--- Lab 07/Lab 07/Program.cs
+++ Lab 07/Lab 07/Program.cs
@@ -12,4 +12,29 @@ namespace Lab_07
         {
         }
     }
+
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+
 }
